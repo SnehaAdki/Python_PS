c68597c87de5800498b3a4c8b0121634029a8792
SnehaAdki <adkisneha1201@gmail.com>
2026-09-17 13:18:44 +0000

    updated the math & round

---
+++ Collections/Math/dem_math.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.sqrt(16))  # Output: 4.0
+print(math.pow(2, 3))  # Output: 8.0
+print(math.pi)  # Output: 3.141592653589793
+print(math.cos(10))  # Output: -0.8390715290764524
+print(math.sin(10))  # Output: 0.5440211108893698
+print(math.fabs(-5.5))  # Output: 5.5
+print(math.factorial(5))  # Output: 120
+
+print(math.floor(4.55555)) #output 4
+print(math.ceil(4.55555)) # output 5
+print(math.log(100, 10)) # output 2.0
+print(math.round(4.55555, 2)) # output 4.56
